c7df2b544b32020ab4b9247050b92ec0a6bd0174
bekozi <benkoziol@gmail.com>
2018-02-28 10:37:01 -0700

    WIP: Preparing for ESMF v7.1.0 release

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 export ESMF_DIR=$(pwd)
+
 export ESMF_INSTALL_PREFIX=${PREFIX}
 export ESMF_INSTALL_BINDIR=${PREFIX}/bin
 export ESMF_INSTALL_DOCDIR=${PREFIX}/doc
 export ESMF_INSTALL_HEADERDIR=${PREFIX}/include
 export ESMF_INSTALL_LIBDIR=${PREFIX}/lib
 export ESMF_INSTALL_MODDIR=${PREFIX}/mod
+
 export ESMF_NETCDF="split"
 export ESMF_NETCDF_INCLUDE=${PREFIX}/include
 export ESMF_NETCDF_LIBPATH=${PREFIX}/lib
-# Needed for mpich-v3 support.
-export ESMF_CXXLINKLIBS=-lmpifort
 
-#export ESMF_COMM=mpiuni
-export ESMF_COMM=mpich2
+export ESMF_COMM=mpich3
 
 make
 #make check
