819a52b8b52d2d46e394e91b647a1611d08458c5
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2023-01-26 13:50:38 +0100

    Try more minimal approach

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,27 +1,27 @@
 set "cwd=%cd%"
 
-set "LIBRARY_PREFIX=%LIBRARY_PREFIX:\=/%"
-set "MINGWBIN=%LIBRARY_PREFIX%/mingw-w64/bin"
+rem set "LIBRARY_PREFIX=%LIBRARY_PREFIX:\=/%"
+rem set "MINGWBIN=%LIBRARY_PREFIX%/mingw-w64/bin"
 
-:: These flags cause errors during CMake; disable for now?
-set "LDFLAGS=-L%LIBRARY_PREFIX%/lib -Wl,-rpath,%LIBRARY_PREFIX%/lib"
-::            Left out: -lcurl -lhdf5 -lhdf5_hl -ldf -lmfhdf
-set "CFLAGS=-fPIC -I%LIBRARY_PREFIX%/include"
-:: PENDING -> PARALLEL MPI STUFF
-:: set PARALLEL=""
+rem :: These flags cause errors during CMake; disable for now?
+rem set "LDFLAGS=-L%LIBRARY_PREFIX%/lib -Wl,-rpath,%LIBRARY_PREFIX%/lib"
+rem ::            Left out: -lcurl -lhdf5 -lhdf5_hl -ldf -lmfhdf
+rem set "CFLAGS=-fPIC -I%LIBRARY_PREFIX%/include"
+rem :: PENDING -> PARALLEL MPI STUFF
+rem :: set PARALLEL=""
 
 set "ESMF_DIR=%cd%"
 
-set "ESMF_INSTALL_PREFIX=%LIBRARY_PREFIX%"
-set "ESMF_INSTALL_BINDIR=%LIBRARY_PREFIX%/bin"
-set "ESMF_INSTALL_DOCDIR=%LIBRARY_PREFIX%/doc"
-set "ESMF_INSTALL_HEADERDIR=%LIBRARY_PREFIX%/include"
-set "ESMF_INSTALL_LIBDIR=%LIBRARY_PREFIX%/lib"
-set "ESMF_INSTALL_MODDIR=%LIBRARY_PREFIX%/mod"
+rem set "ESMF_INSTALL_PREFIX=%LIBRARY_PREFIX%"
+rem set "ESMF_INSTALL_BINDIR=%LIBRARY_PREFIX%/bin"
+rem set "ESMF_INSTALL_DOCDIR=%LIBRARY_PREFIX%/doc"
+rem set "ESMF_INSTALL_HEADERDIR=%LIBRARY_PREFIX%/include"
+rem set "ESMF_INSTALL_LIBDIR=%LIBRARY_PREFIX%/lib"
+rem set "ESMF_INSTALL_MODDIR=%LIBRARY_PREFIX%/mod"
 
-set "ESMF_NETCDF=split"
-set "ESMF_NETCDF_INCLUDE=%LIBRARY_PREFIX%/include"
-set "ESMF_NETCDF_LIBPATH=%LIBRARY_PREFIX%/lib"
+rem set "ESMF_NETCDF=split"
+rem set "ESMF_NETCDF_INCLUDE=%LIBRARY_PREFIX%/include"
+rem set "ESMF_NETCDF_LIBPATH=%LIBRARY_PREFIX%/lib"
 
 
 rem if [[ "$(echo $fortran_compiler_version | cut -d '.' -f 1)" -gt 9 ]]; then
@@ -33,15 +33,10 @@ rem fi
 
 :: echo ESMF_F90COMPILEOPTS=${ESMF_F90COMPILEOPTS}
 
-set "ESMF_CPP=%MINGWBIN%/cpp.exe -E -P -x c"
+rem set "ESMF_CPP=%MINGWBIN%/cpp.exe -E -P -x c"
 
-set "ESMF_F90=%MINGWBIN%/gfortran.exe"
-set "ESMF_CXX=%MINGWBIN%/g++.exe"
-
-rem set "ESMF_COMM=mpiuni"
-
-set "ESMF_OS=MinGW"
-rem set "ESMF_COMPILER=gfortran"
+rem set "ESMF_F90=%MINGWBIN%/gfortran.exe"
+rem set "ESMF_CXX=%MINGWBIN%/g++.exe"
 
 mingw32-make info
 
